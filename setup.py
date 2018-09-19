563c17c75518ee0491dfa9879f5fa32accc1608d
Jeroen Dhollander <jeroen.dhollander@gmail.com>
2018-09-18 20:36:18 -0700

    Add URL to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version="0.1.0",
     description="Library for adding bash 'more' like functionality to your Python application",
     author="Jeroen Dhollander",
+    url='https://github.com/jeroen-dhollander/python-paginator',
     packages=find_packages(),
     include_package_data=True,
 )
