fb9c98b1fb610b25294afa17a497ab28416b9dfd
Jeroen Dhollander <jeroen.dhollander@gmail.com>
2018-09-18 20:57:17 -0700

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!python
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='paginator',
     version="0.1.0",
-    description="Library for adding bash 'more' like functionality to your Python application",
+    description="Library for adding 'more' like paging functionality to your Python application",
+    long_description=long_description,
     author="Jeroen Dhollander",
     url='https://github.com/jeroen-dhollander/python-paginator',
     packages=find_packages(),
