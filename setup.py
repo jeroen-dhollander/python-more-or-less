5eed79bf0d62fb23a5de10a99811a042d93094a4
Jeroen Dhollander <jeroen.dhollander@gmail.com>
2018-09-24 08:47:41 -0700

    Bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='more-or-less',
-    version="0.1.2",
+    version="0.1.3",
     description="Library for adding 'more' like paging functionality to your Python application",
     long_description=long_description,
     author="Jeroen Dhollander",
